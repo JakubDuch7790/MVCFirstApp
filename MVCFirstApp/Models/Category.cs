6bea5ac761781d3aab728689627b61028f928913
Jakub Duch <jakub.duch23@gmail.com>
2023-09-04 22:51:23 +0200

    Added Server side validation for creating category

--- MVCFirstApp/Models/Category.cs
+++ MVCFirstApp/Models/Category.cs
@@ -9,8 +9,10 @@ namespace MVCFirstApp.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage = "Invalid number")]
         public int DisplayOrder { get; set; }
     }
 }
