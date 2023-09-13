7cf388fd44054cd32f676cbb8d59f424528270e4
Jakub Duch <jakub.duch23@gmail.com>
2023-09-13 12:33:37 +0200

    Added funcionality in order to be able to display all categories in Razor Project

--- RazorFirstApp/Pages/Categories/Index.cshtml.cs
+++ RazorFirstApp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorFirstApp.Data;
+using RazorFirstApp.Models;
 
 namespace RazorFirstApp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
