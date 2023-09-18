50cef66fbc37b1dfeb9604805846506730a385e6
Jakub Duch <jakub.duch23@gmail.com>
2023-09-18 15:53:27 +0200

    Database Reset

--- MVCFirstApp.DataAcces/Migrations/20230918135005_AddTableToDbAndSeed.cs
+++ MVCFirstApp.DataAcces/Migrations/20230918135005_AddTableToDbAndSeed.cs
@@ -2,10 +2,12 @@
 
 #nullable disable
 
-namespace MVCFirsMVCFirstApp.DataAccestApp.Migrations
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MVCFirstApp.DataAcces.Migrations
 {
     /// <inheritdoc />
-    public partial class AddCategoryTableToDb : Migration
+    public partial class AddTableToDbAndSeed : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,12 +18,24 @@ namespace MVCFirsMVCFirstApp.DataAccestApp.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
                     DisplayOrder = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Alfa" },
+                    { 2, 2, "Beta" },
+                    { 3, 3, "Gama" },
+                    { 4, 4, "Delta" },
+                    { 5, 5, "Epsilon" }
                 });
         }
 
--- MVCFirstApp.DataAcces/Migrations/ApplicationDbContextModelSnapshot.cs
+++ MVCFirstApp.DataAcces/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -16,7 +16,7 @@ namespace MVCFirstApp.DataAcces.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.7")
+                .HasAnnotation("ProductVersion", "7.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -34,7 +34,8 @@ namespace MVCFirstApp.DataAcces.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                        .HasMaxLength(30)
+                        .HasColumnType("nvarchar(30)");
 
                     b.HasKey("Id");
 
