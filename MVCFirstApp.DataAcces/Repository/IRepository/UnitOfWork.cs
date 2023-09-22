fa7cef1adaa46bc4b6770397cf3b9f698cc5470e
Jakub Duch <jakub.duch23@gmail.com>
2023-09-22 17:19:45 +0200

    UnitOfWork Implementation (Added IUnitOfWork interface, UnitOfWork class)

---
+++ MVCFirstApp.DataAcces/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using MVCFirstApp.DataAcces.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCFirstApp.DataAcces.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICategoryRepository CategoryRepository { get;private set; }
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db, CategoryRepository categoryRepository)
+        {
+            CategoryRepository = categoryRepository;
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
