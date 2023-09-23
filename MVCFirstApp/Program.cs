deaae8267f5e83293ac9e9e7173d1628d2a7a376
Jakub Duch <jakub.duch23@gmail.com>
2023-09-23 23:50:02 +0200

    Added route

--- MVCFirstApp/Program.cs
+++ MVCFirstApp/Program.cs
@@ -33,6 +33,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:Host}/{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
