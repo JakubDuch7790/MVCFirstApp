cf00d85bf72d13e32bdb80aa588535e226555bc0
Jakub Duch <jakub.duch23@gmail.com>
2023-09-27 17:48:23 +0200

    Product class innit

--- MVCFirstApp.Models/Product.cs
+++ MVCFirstApp.Models/Product.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVCFirstApp.Models
 {
-    internal class Product
+    public class Product
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Brand")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+
     }
 }
