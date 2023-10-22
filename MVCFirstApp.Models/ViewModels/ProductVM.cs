d405b9f91d9690f9ab845341625a3de8c2200347
Jakub Duch <jakub.duch23@gmail.com>
2023-10-22 13:37:17 +0200

    Added ViewModels import statement

--- MVCFirstApp.Models/ViewModels/ProductVM.cs
+++ MVCFirstApp.Models/ViewModels/ProductVM.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVCFirstApp.Models.ViewModels
+namespace MVCFirstApp.Models.ViewModels;
+
+public class ProductVM
 {
-    public class ProductVM
-    {
-    }
+    public Product Product { get; set; }
+    public IEnumerable<SelectListItem> CategoryList { get; set; }
 }
