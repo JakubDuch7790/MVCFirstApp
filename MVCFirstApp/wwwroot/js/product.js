5780ae59ae54e8e8f6a40cbef7e7a66ed5d4ef88
Jakub Duch <jakub.duch23@gmail.com>
2023-11-10 16:12:04 +0100

    product.js added loading datatables in progress

---
+++ MVCFirstApp/wwwroot/js/product.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () { loadDataTable(); });
+
+
+
+$('#myTable').DataTable({
+    ajax: '/api/myData'
+});
+
+function loadDataTable() {
+
+    dataTable = $('#tableData').DataTable({
+        "ajax": { url: '/admin/product/getall'},
+        "columns": [
+        { data: 'name', "width": "15%" },
+        { data: 'position', "width": "15%" },
+        { data: 'salary', "width": "15%" },
+        { data: 'office', "width": "15%" }
+    ]);
+
+}
