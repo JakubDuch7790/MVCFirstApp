3e6c856edcf62b552ed6b64cc72d4794e9d25272
Jakub Duch <jakub.duch23@gmail.com>
2023-11-10 16:36:02 +0100

    Load Datatables Data vanished

--- MVCFirstApp/wwwroot/js/product.js
+++ MVCFirstApp/wwwroot/js/product.js
@@ -1,20 +1,17 @@
 ﻿$(document).ready(function () { loadDataTable(); });
 
-
-
-$('#myTable').DataTable({
-    ajax: '/api/myData'
-});
-
 function loadDataTable() {
 
     dataTable = $('#tableData').DataTable({
         "ajax": { url: '/admin/product/getall'},
         "columns": [
-        { data: 'name', "width": "15%" },
-        { data: 'position', "width": "15%" },
-        { data: 'salary', "width": "15%" },
-        { data: 'office', "width": "15%" }
+        { data: 'brand', "width": "15%" },
+        { data: 'yearOfConstruction', "width": "15%" },
+        { data: 'kilometresDriven', "width": "15%" },
+        { data: 'powerInKilowatts', "width": "15%" },
+        { data: 'price', "width": "15%" },
+        { data: 'category.name', "width": "15%" },
+
     ]);
 
 }
