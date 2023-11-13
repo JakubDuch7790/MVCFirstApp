3206e9f196ae47e3414d935da1ebd59fe6e11575
Jakub Duch <jakub.duch23@gmail.com>
2023-11-13 15:09:28 +0100

    added Delete btn

--- MVCFirstApp/wwwroot/js/product.js
+++ MVCFirstApp/wwwroot/js/product.js
@@ -6,11 +6,11 @@ function loadDataTable() {
         "ajax": { url: '/Admin/Product/Getall' },
         "columns": [
             { data: 'brand', "width": "15%" },
-            { data: 'price', "width": "15%" },
+            { data: 'price', "width": "10%" },
             { data: 'kilometresDriven', "width": "15%" },
-            { data: 'powerInKilowatts', "width": "15%" },
-            { data: 'yearOfConstruction', "width": "15%" },
-            { data: 'category.name', "width": "15%" },
+            { data: 'powerInKilowatts', "width": "10%" },
+            { data: 'yearOfConstruction', "width": "5%" },
+            { data: 'category.name', "width": "10%" },
 
             {
                 data: 'id',
@@ -19,12 +19,14 @@ function loadDataTable() {
                     return `<div class="w-auto btn-group" role="group">
                     <a href="" class="btn btn-primary mx-auto">
                                     <i class="bi bi-pencil-square"> Edit</i>
-                                </a>
+                    </a>
+                    <a class="btn btn-danger mx-auto">
+                        <i class="bi bi-trash3-fill"> Delete</i>
+                    </a>
                     </div>`
                 },
 
-                "width": "15%"
-
+                "width": "20%"
             },
         ]
         })
