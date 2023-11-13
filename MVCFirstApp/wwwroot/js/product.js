6a6e2307d52169c6b6e3ad24424facf09f959cdd
Jakub Duch <jakub.duch23@gmail.com>
2023-11-13 14:58:26 +0100

    added Edit btn

--- MVCFirstApp/wwwroot/js/product.js
+++ MVCFirstApp/wwwroot/js/product.js
@@ -6,11 +6,26 @@ function loadDataTable() {
         "ajax": { url: '/Admin/Product/Getall' },
         "columns": [
             { data: 'brand', "width": "15%" },
-            { data: 'yearOfConstruction', "width": "15%" },
+            { data: 'price', "width": "15%" },
             { data: 'kilometresDriven', "width": "15%" },
             { data: 'powerInKilowatts', "width": "15%" },
-            { data: 'price', "width": "15%" },
+            { data: 'yearOfConstruction', "width": "15%" },
             { data: 'category.name', "width": "15%" },
+
+            {
+                data: 'id',
+
+                "render": function (data) {
+                    return `<div class="w-auto btn-group" role="group">
+                    <a href="" class="btn btn-primary mx-auto">
+                                    <i class="bi bi-pencil-square"> Edit</i>
+                                </a>
+                    </div>`
+                },
+
+                "width": "15%"
+
+            },
         ]
         })
 };
