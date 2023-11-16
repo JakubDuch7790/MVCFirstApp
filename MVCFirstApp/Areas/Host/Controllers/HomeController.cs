11b8c5ba787e83e395b6e8865f20cbe1fc092039
Jakub Duch <jakub.duch23@gmail.com>
2023-11-16 13:07:06 +0100

    Home controller debugg

--- MVCFirstApp/Areas/Host/Controllers/HomeController.cs
+++ MVCFirstApp/Areas/Host/Controllers/HomeController.cs
@@ -11,14 +11,15 @@ namespace MVCFirstApp.Areas.Host.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includedProperties:"Category");
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includedProperties: "Category");
 
             return View(products);
         }
