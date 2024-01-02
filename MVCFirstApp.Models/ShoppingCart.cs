3f1332457d16678c35e657d82045b05c7b6bfc71
Jakub Duch <jakub.duch23@gmail.com>
2024-01-02 22:57:26 +0100

    ShoppingCart Update

--- MVCFirstApp.Models/ShoppingCart.cs
+++ MVCFirstApp.Models/ShoppingCart.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,17 @@ namespace MVCFirstApp.Models
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        [Range (1, 3, ErrorMessage ="You have reached the limit, If you want to buy more please open another transaction.")]
         public int Count { get; set; }
         public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+
+
+
     }
 }
