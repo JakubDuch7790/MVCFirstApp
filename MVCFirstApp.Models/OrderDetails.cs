acdd4820090ca81d97f3f411388c8edeb26927d2
Jakub Duch <jakub.duch23@gmail.com>
2024-01-28 23:39:42 +0100

    OrderDetails and  OrderHeader  added to project

---
+++ MVCFirstApp.Models/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCFirstApp.Models;
+
+public class OrderDetails
+{
+    public int Id { get; set; }
+    public int ApplicationUserId { get; set; }
+    [ForeignKey(nameof(ApplicationUserId))]
+    [ValidateNever]
+    public ApplicationUser ApplicationUser { get; set; }    
+}
