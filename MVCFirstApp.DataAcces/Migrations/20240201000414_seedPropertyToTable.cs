3bb45b55c5d1027e8baf084206712e43da7f8fc6
Jakub Duch <jakub.duch23@gmail.com>
2024-02-01 01:22:32 +0100

    Added Helper methods in OH repo OH model update

---
+++ MVCFirstApp.DataAcces/Migrations/20240201000414_seedPropertyToTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCFirstApp.DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedPropertyToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Session",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Session",
+                table: "OrderHeaders");
+        }
+    }
+}
---
+++ MVCFirstApp.DataAcces/Migrations/20240201001821_ColumnRename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCFirstApp.DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class ColumnRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Session",
+                table: "OrderHeaders",
+                newName: "SessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SessionId",
+                table: "OrderHeaders",
+                newName: "Session");
+        }
+    }
+}
