169f88e8a9432349add4c0661c67e0024962d9b7
Jakub Duch <jakub.duch23@gmail.com>
2024-02-07 23:58:56 +0100

    midnight commit

---
+++ MVCFirstApp/wwwroot/js/order.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () { loadDataTable(); });
+
+function loadDataTable() {
+
+    dataTable = $('#tableData').DataTable({
+        "ajax": { url: '/Admin/Order/Getall' },
+        "columns": [
+            { data: 'id', "width": "15%" },
+            { data: 'name', "width": "5%" },
+            { data: 'phoneNumber', "width": "10%" },
+            { data: 'applicationUser.email', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
+            { data: 'orderTotal', "width": "5%" },
+
+            {
+                data: 'id',
+
+                "render": function (data) {
+                    return `<div class="w-auto btn-group" role="group">
+                    <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-auto">
+                                    <i class="bi bi-pencil-square"> Edit</i>
+                    </a>
+                    </div>`
+                },
+
+                "width": "20%"
+            },
+        ]
+        })
+};
