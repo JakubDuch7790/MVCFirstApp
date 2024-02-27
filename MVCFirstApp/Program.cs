cb67466671ab9d2df4736b368151f8eb5335c6bb
Jakub Duch <jakub.duch23@gmail.com>
2024-02-27 01:09:39 +0100

    app session configuration

--- MVCFirstApp/Program.cs
+++ MVCFirstApp/Program.cs
@@ -28,6 +28,14 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 
 builder.Services.AddRazorPages();
 //Register service to dependency injection
@@ -54,6 +62,7 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
 app.MapRazorPages();
 
 app.MapControllerRoute(
